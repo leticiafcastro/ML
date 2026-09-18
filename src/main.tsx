881067c413739488b53add9150e3a991d6e37073
leticiafcastro <140448722+leticiafcastro@users.noreply.github.com>
2026-09-18 18:51:52 -0300

    chore: add initialization comment in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Validação e inicialização da aplicação: Modelo Lógico - Dispensação de Medicamentos
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
